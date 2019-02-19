043276e359e9e130031e423b76e43f10c5223f76
unknown <ajaydevofficial@gmail.com>
2019-02-19 12:48:36 +0530

    Login

--- hostelauto/hostelauto/views.py
+++ hostelauto/hostelauto/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import get_user_model,authenticate,login
 
 def home_page(request):
     context = {}
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password=password)
+        if not user==None:
+            login(request,user)
+            return redirect(home_page)
+        else:
+            print("Login Failed")
+            return render(request,"index.html",{"invalid":True})
+
     return render(request,"index.html",context)
 
 def menu_page(request):
